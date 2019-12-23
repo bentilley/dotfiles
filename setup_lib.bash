312f65a50882a1757997f0e3a09b059a7044936e
Benjamin Tilley <bentilley@ingresso.co.uk>
2019-12-23 16:44:10 +0000

    Add colours to install script and add vim symlink

--- setup_lib.bash
+++ setup_lib.bash
@@ -1,12 +1,22 @@
 DOTFILE_DIR="$HOME/.dotfiles"
 
+# colours
+blue=$(tput setaf 4)
+green=$(tput setaf 2)
+normal=$(tput sgr0)
+
 dotfile_link() {
   SOURCE_FILE=$DOTFILE_DIR/"$1"
   TARGET_FILE="$2"
 
+  test_target_file_dir $TARGET_FILE
+
   if [ -f $SOURCE_FILE -o -d $SOURCE_FILE ]; then
     if [ ! -h $TARGET_FILE ]; then
       ln -s $SOURCE_FILE $TARGET_FILE
+      if [ $? -eq 0 ]; then
+        symlink_success $SOURCE_FILE $TARGET_FILE
+      fi
     else
       symlink_error $TARGET_FILE "Symlink already exists"
     fi
@@ -15,6 +25,17 @@ dotfile_link() {
   fi
 }
 
+test_target_file_dir() {
+  TARGET_DIR=$(dirname "$1")
+  if [ ! -d $TARGET_DIR ]; then
+    mkdir -p $TARGET_DIR
+  fi
+}
+
+symlink_success() {
+  printf "  ${green}Linking${normal} $1 to $2 :)\n"
+}
+
 symlink_error() {
-  printf "Did not link file $1. $2.\n"
+  printf "  Did not link file $1. ${blue}$2.${normal}\n"
 }
