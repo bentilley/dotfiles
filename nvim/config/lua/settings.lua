875e063521783a73201282855e0a8c6241f2c88c
bentilley <tilleyben@hotmail.com>
2022-07-09 15:05:18 +0100

    feat(nvim): update settings

--- nvim/config/lua/settings.lua
+++ nvim/config/lua/settings.lua
@@ -3,71 +3,50 @@
 -- Created: 2022-05-08
 -- Neovim Settings & Configuration
 
--- constants
-
-local opts = vim.opt
-local g = vim.g
-
-
 -- boolean options
 
-opts.autoread       = true
-opts.autoindent     = true
--- opts.equalalways    = false
-opts.expandtab      = true
-opts.hidden         = true
-opts.hlsearch       = true
-opts.ignorecase     = true
-opts.incsearch      = true
-opts.lazyredraw     = true
-opts.modeline       = true
-opts.number         = true
-opts.relativenumber = true
-opts.ruler          = true
-opts.shiftround     = true
-opts.showmatch      = true
-opts.showmode       = true
-opts.showcmd        = true
-opts.smartcase      = true
-opts.smartindent    = true
-opts.wildmenu       = true
--- opts.termguicolors  = true
-
+vim.opt.autoindent = true
+vim.opt.autoread = true
+-- vim.opt.equalalways    = false
+vim.opt.expandtab = true
+vim.opt.hidden = true
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+vim.opt.lazyredraw = true
+vim.opt.modeline = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ruler = true
+vim.opt.shiftround = true
+vim.opt.showcmd = true
+vim.opt.showmatch = true
+vim.opt.showmode = true
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+-- vim.opt.tagrelative = false
+-- vim.opt.termguicolors  = true
+vim.opt.undofile = true
+vim.opt.wildmenu = true
 
 -- non-boolean options
 
--- vim.api.nvim_command([[filetype plugin indent on]])
-
-opts.backspace      = 'indent,eol,start'
-opts.completeopt    = "menu,menuone,noselect,preview"
-opts.formatoptions  = 'tcqj1'
-opts.foldlevelstart = 99
-opts.foldmethod     = 'indent'
-opts.shiftwidth     = 2
-opts.softtabstop    = 2
-opts.scrolloff      = 2
-opts.statusline     = '%< %n:%f %m%r%y%=%-35.(line: %l of %L, col: %c%V (%P)%)'
--- opts.syntax         = 'on'
-opts.tabstop        = 4
-opts.wildmode       = 'longest,full'
--- opts.wildignore     = '*/tmp/*,*.so,*.swp,*.zip,*/node_modules/*'
-
-
--- other variables
-
-g.mapleader = '<Space>'
-g.maplocalleader = ';'
--- NOTE: plugin clever-f re-maps original ';' usage to multiple f presses
-
--- colorscheme squeedom
--- set complete-=i                         " removes indluded files from search
--- set listchars=eol:$,space:.,tab:<->,trail:%,precedes:<,extends:>
--- set notagrelative                        " tags not followed relative to tag file
--- set path=$PWD/**
--- set sidescroll=5
--- set tags+=./.git/tags
--- set timeoutlen=1000 ttimeoutlen=0        " exit visual faster
--- set undodir^=~/.config/vim/extra/undo//  " set a directory for undo files
--- set undofile
--- set viewdir=~/.config/vim/views          " set a directory for view files
-
+vim.opt.backspace = { "indent", "eol", "start" }
+vim.opt.complete = vim.opt.complete - "i"
+vim.opt.completeopt = { "menu", "menuone", "noselect", "preview" }
+vim.opt.foldlevelstart = 99
+vim.opt.foldmethod = "indent"
+vim.opt.formatoptions = "tcqj1"
+vim.opt.listchars = { eol = "$", space = ".", tab = "<->", trail = "%", precedes = "<", extends = ">" }
+vim.opt.path = vim.opt.path + (vim.env.PWD .. "/**")
+vim.opt.scrolloff = 2
+vim.opt.shiftwidth = 2
+vim.opt.sidescroll = 5
+vim.opt.softtabstop = 2
+vim.opt.statusline = "%< %n:%f %m%r%y%=%-35.(line: %l of %L, col: %c%V (%P)%)"
+vim.opt.tabstop = 4
+vim.opt.tag = vim.opt.tag + "./.git/tags"
+vim.opt.timeoutlen = 500
+vim.opt.ttimeoutlen = 0
+vim.opt.wildmode = { "longest", "full" }
+-- vim.opt.wildignore     = '*/tmp/*,*.so,*.swp,*.zip,*/node_modules/*'
