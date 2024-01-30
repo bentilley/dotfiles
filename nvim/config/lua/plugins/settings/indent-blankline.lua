f30001df2591582a4343e94682d9a3e2ee20d92e
bentilley <tilleyben@hotmail.com>
2024-01-30 12:28:52 +0000

    fix(nvim): update setup config for indent-blankline plugin

--- nvim/config/lua/plugins/settings/indent-blankline.lua
+++ nvim/config/lua/plugins/settings/indent-blankline.lua
@@ -5,9 +5,4 @@
 
 -- settings
 
-require("indent_blankline").setup({
-	show_current_context = true,
-	-- show_current_context_start = true,
-	use_treesitter = true,
-	use_treesitter_scope = true,
-})
+require("ibl").setup()
